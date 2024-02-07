8af2b43f5361beb6d8cb2164e8a89877eb741a0f
Thiebaud Reimann <thiebaudreimann@gmail.com>
2024-02-07 22:14:54 +0100

    json blog article

--- js/main.js
+++ js/main.js
@@ -32,5 +32,62 @@ function loadTeamGrid() {
         });
 }
 
-loadTeamGrid();
+/**
+ * ARCHIV
+ */
+function loadBlog() {
+    const jsonFilePath = '../json/blog.json';
+
+    const myTab = document.getElementById('myTab');
+    const myTabContent = document.getElementById('myTabContent');
+
+    fetch(jsonFilePath)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach((entry, index) => {
+                const isActive = index === 0;
+
+                // Button
+                const newListItem = document.createElement('li');
+                newListItem.classList.add('nav-item');
+                newListItem.setAttribute('role', 'presentation');
+                const newButtonHTML = `<button class="nav-link ${isActive ? 'active' : ''}" id="${entry.shortId}-tab" data-bs-toggle="tab" data-bs-target="#${entry.shortId}-tab-pane" type="button" role="tab" aria-controls="${entry.shortId}-tab-pane" aria-selected="${isActive}">${entry.jahr}</button>`;
+                newListItem.innerHTML = newButtonHTML;
+
+                // Content
+                const newTabContentItem = document.createElement('div');
+                newTabContentItem.classList.add('tab-pane');
+                newTabContentItem.classList.add('fade');
+                if (isActive) {
+                    newTabContentItem.classList.add('show');
+                    newTabContentItem.classList.add('active');
+                }
+                newTabContentItem.id = `${entry.shortId}-tab-pane`;
+                newTabContentItem.setAttribute('role', 'tabpanel');
+                newTabContentItem.setAttribute('aria-labelledby', `${entry.shortId}-tab`);
+                
+                const langGerman = window.location.href.endsWith('de/');
+
+                const text = `
+                <h1>${entry.name} <img src="${entry.saisonIcon}" height="40px"></h1>
+                <br/>
+                ${langGerman ? entry.text['de'] : entry.text['en']}
+                `;
+
+                newTabContentItem.innerHTML = text;
+                // Add to HTML
+                myTab.appendChild(newListItem);
+                myTabContent.appendChild(newTabContentItem);
+            });
+        })
+        .catch(error => {
+            console.error('Error loading the JSON file:', error);
+        });
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    loadTeamGrid();
+    loadBlog();
+});
 
